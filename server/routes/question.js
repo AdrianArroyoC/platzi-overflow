9371a18f937b1cec18286d55b1045fe35f6495d8
Adrian Arroyo Ceja <adrianarroyoceja@users.noreply.github.com>
2019-02-18 17:57:55 -0600

    Commit hasta obteniendo una pregunta

--- server/routes/question.js
+++ server/routes/question.js
@@ -1,24 +1,32 @@
 import express from 'express'
-import {
-    required,
-    questionMiddleware,
-    questionsMiddleware
-} from '../middleware'
+import { required } from '../middleware'
+import { question } from '../db-api'
+import { handleError } from '../utils'
 
 const app = express.Router()
 
 // GET /api/questions
-app.get('/', questionsMiddleware, (req, res) => {
-    // setTimeout(() => {
-        res.status(200).json(req.questions)
-    // }, 2000)
+app.get('/', async (req, res) => {
+    try {
+        const questions = await question.findAll()
+        // setTimeout(() => {
+        res.status(200).json(questions)
+        // }, 2000)
+    } catch (err) {
+        handleError(err, res)
+    }
 })
 
 // GET /api/questions/:id
-app.get('/:id', questionMiddleware, (req, res) => {
+app.get('/:id', async (req, res) => {
     /*const { id } = req.params
     const q = questions.find(({ _id }) => _id === +id)*/
-    res.status(200).json(req.question)
+    try {
+        const q = await question.findById(req.params.id)
+        res.status(200).json(q)
+    } catch (err) {
+        handleError(err, res)
+    }
 })
 
 // POST /api/questions
@@ -38,7 +46,7 @@ app.post('/', required, (req, res) => {
     res.status(201).json(question)
 })
 
-app.post('/:id/answers', questionMiddleware, required, (req, res) => {
+app.post('/:id/answers', required, (req, res) => {
     const answer = req.body
     const q = req.question
     answer.createdAt = new Date()
